33b60d8e347963b3084155c7dfa0cc535c59af21
Goulwen COURTAUX <goulwencourtaux@MacBook-Pro-de-Goulwen.home>
2020-04-02 18:18:57 +0200

    add device-detect feature to disable site on mobile for now

--- src/index.js
+++ src/index.js
@@ -7,9 +7,25 @@ import * as serviceWorker from './serviceWorker'
 import store from './store/store'
 import { Provider } from 'react-redux'
 
+import {
+    BrowserView,
+    MobileView
+  } from "react-device-detect";
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <MobileView>
+            <div className="valign-wrapper absolute">
+            <div className="valign-inner">
+                <h1>WHO's<br />WHO?</h1>
+                <h3>Ce site n'est pas optimisé pour mobile</h3>
+                <h4>Bisou...</h4>
+            </div>
+            </div>
+        </MobileView>
+        <BrowserView>
+            <App />
+        </BrowserView>
     </Provider>, document.getElementById('root')
 );
 
